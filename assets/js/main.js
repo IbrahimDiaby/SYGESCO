105ab6c72d9e6b25677fa73179e9fd751c8f952c
NID <nid@localhost.localdomain>
2025-11-26 20:34:25 +0100

    [ADD] : Search cancel feature added with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const searchCancel = document.querySelector(
+    ".search-input-container :nth-child(3)"
+  );
+  const search = document.querySelector("#search");
+
+  search.addEventListener("change", () => {
+    searchCancel.style.display = search.value.length === 0 ? "none" : "block";
+  });
+
+  search.addEventListener("keydown", (e) => {
+    search.setAttribute("value", e.target.value);
+    searchCancel.style.display = search.value.length === 0 ? "none" : "block";
+  });
+
+  searchCancel.addEventListener("click", () => {
+    search.value = "";
+    searchCancel.style.display = search.value.length === 0 ? "none" : "block";
+  });
 });
 
 window.addEventListener("focus", () => {
